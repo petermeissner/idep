cb61991a2d439643848791e617d338f21eab913a
petermeissner <retep.meissner@gmail.com>
2014-11-19 23:45:55 +0100

    minor: added downoad task

--- R/get_ready.R
+++ R/get_ready.R
@@ -3,20 +3,6 @@
 
 get_ready <- function(){
   message("sourcing sql functions")
-  source("http://mbtnr.net/rsource/sqlFunctions.r")
-  source("http://mbtnr.net/rsource/idep/sqlFunctionsExtended.r")
   source("D:/Users/Peter/DBConnections/DBSOconnection.r")
-  sqlEscape <<- function(data){
-    iffer <- is.na(data)
-    data  <- paste0( "'", dbEscapeStrings(socon, as.character(data)) , "'")
-    data[iffer] <- "NULL"
-    data
-  }
-  dbGetQueries <<- function(con, SQL){
-    dummyfunc <- function(sql){dbGetQuery(con, sql)}
-    res <- unlist(plyr::llply(SQL, dummyfunc, .progress="text"))
-    res
-  }
-    
   setwd("Z:/Gesch\u00e4ftsordnungen/AggregatedData/working")
 }
