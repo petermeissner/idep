5e67403d72e1c505db61018eb28381424e9fed4a
petermeissner <retep.meissner@gmail.com>
2015-02-23 17:40:59 +0100

    corpus_file_select() and link_file_select() accept file-lists as well

--- R/corpus_file_select.R
+++ R/corpus_file_select.R
@@ -1,9 +1,12 @@
 #' function for selecting a bunch of link files and computing other vars
 #' 
 
-corpus_file_select <- function() {
-  corpus_file_full  <<- choose.files()
-  corpus_file_full  <<- stringr::str_replace_all(corpus_file_full,"\\\\","/")
-  corpus_file_fname <<- basename(corpus_file_full)
-  corpus_file_path  <<- dirname(corpus_file_full)
+corpus_file_select <- function(corpus_file_full=NULL) {
+  if ( is.null(corpus_file_full) ){
+    corpus_file_full <- choose.files()
+  }
+  corpus_file_full <- stringr::str_replace_all(corpus_file_full,"\\\\","/")
+  assign("corpus_file_full", corpus_file_full, envir=globalenv() )
+  assign("corpus_file_fname", basename(corpus_file_full), envir=globalenv() )
+  assign("corpus_file_path",  dirname(corpus_file_full), envir=globalenv() )
 }
