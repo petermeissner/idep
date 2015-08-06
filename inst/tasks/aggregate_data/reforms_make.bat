ad4a56177033de623b5b70480727f28c28a1577a
Peter Meissner <retep.meissner@gmail.com>
2015-08-06 15:18:48 +0200

    aggregate reforms

--- inst/tasks/aggregate_data/reforms_make.bat
+++ inst/tasks/aggregate_data/reforms_make.bat
@@ -1,9 +1,11 @@
-Rscript -e "library(idep); file.move(grep('Rmd$|bat$|r$', list.files(pattern='.*\\..*'), invert=T, value=T, ignore.case=T),'old')"
-REM Rscript -e "library(knitr); knitr::stitch_rhtml('reforms.R', 'reforms.R.out.html')"
+Rscript -e "library(idep); file.move(grep('Rmd$|bat$|r$|pdf$', list.files(pattern='.*\\..*'), invert=T, value=T, ignore.case=T),'old')"
+Rscript -e "unlink(list.files(pattern='tex2pdf'), recursive=T)"
+Rscript -e "library(knitr); knitr::stitch_rhtml('reforms.R', 'reforms.R.out.html')"
 Rscript -e "library(knitr); knitr::knit2html('reforms_codebook.Rmd')" 
 Rscript -e "files <- grep('codebook', list.files(pattern='pdf$|html$'), value=T)" ^
-        -e "file.copy(files, 'Z:/Gesch\u00e4ftsordnungen/Database/aggregats')" ^
-        -e "file.copy(files, 'Z:/Gesch\u00e4ftsordnungen/Database/outputs')" 
+        -e "file.copy(files, 'Z:/Gesch\u00e4ftsordnungen/Database/aggregats', overwrite=TRUE)" ^
+        -e "file.copy(files, 'Z:/Gesch\u00e4ftsordnungen/Database/outputs',   overwrite=TRUE)" ^
+        -e "file.copy(files, 'C:/Dropbox/idep/data', overwrite=TRUE)"
 
 echo done & pause
         
