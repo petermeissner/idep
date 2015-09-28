b2ae51f9e5a43595c59c4f2d76603c35520c05c9
Peter Meissner <retep.meissner@gmail.com>
2015-09-28 16:16:07 +0200

    restructure task: prepare external datasets

--- inst/tasks/prepare_erd_dataset/_prepare_min_maj_data.bat
+++
@@ -1,5 +0,0 @@
-cd C:/Dropbox/RPackages/idep/inst/tasks/prepare_dataset_for_pro_min_maj_analysis/
-
-R < prepare_erd_dataset.R              --vanilla
-
-echo. && echo. && echo.--DONE-- && echo. && echo. & pause 
