8184a183ecefefbd8eee943ea62c79e7fd0e45aa
Peter Meissner <retep.meissner@gmail.com>
2015-11-25 08:28:26 +0100

    make.bat (overhaul)

--- inst/tasks/aggregate_data/_isor_isom_make.bat
+++ inst/tasks/aggregate_data/_isor_isom_make.bat
@@ -1,45 +1,45 @@
-prompt $g 
+::prompt $g 
 
 REM ===========================================================================
 REM CLEANING UP
 REM ===========================================================================
 
-  Rscript -e "library(idep); file.move(grep('Rmd$|bat$|r$|pdf$', list.files(pattern='.*\\..*'), invert=T, value=T, ignore.case=T),'old')"
-  Rscript -e "unlink(list.files(pattern='tex2pdf'), recursive=T)"
+CALL Rscript -e "library(idep); file.move(grep('Rmd$|bat$|r$|pdf$', list.files(pattern='.*\\..*'), invert=T, value=T, ignore.case=T),'old')"
+CALL Rscript -e "unlink(list.files(pattern='tex2pdf'), recursive=T)"
 
   
 REM ===========================================================================  
 REM GENERATING isor data set
 REM ===========================================================================
 
-R < isor.R                         > routs/isor.rout                         --vanilla || (PAUSE && EXIT /B 1)
+CALL R < isor.R                         > routs/isor.rout                         --vanilla 
 
-R < isor_ext_pro_minmaj.R          > routs/isor_ext_pro_minmaj.Rout          --vanilla || (PAUSE && EXIT /B 1)
-R < isor_ext_tsebelis.R            > routs/isor_ext_tsebelis.Rout            --vanilla || (PAUSE && EXIT /B 1)
-R < isor_ext_erd_cabinet_ids.R     > routs/isor_ext_erd_cabinet_ids.Rout     --vanilla || (PAUSE && EXIT /B 1)
+CALL R < isor_ext_pro_minmaj.R          > routs/isor_ext_pro_minmaj.Rout          --vanilla 
+CALL R < isor_ext_tsebelis.R            > routs/isor_ext_tsebelis.Rout            --vanilla 
+CALL R < isor_ext_erd_cabinet_ids.R     > routs/isor_ext_erd_cabinet_ids.Rout     --vanilla 
 
 
 REM ===========================================================================  
 REM GENERATING isom data set
 REM ===========================================================================
 
-R < isom.R                         > routs/isom.rout                         --vanilla || (PAUSE && EXIT /B 1)
+CALL R < isom.R                         > routs/isom.rout                         --vanilla 
 
 REM ===========================================================================
 REM building codebook HTML 
 REM ===========================================================================
 
-::Rscript -e "library(knitr); knitr::knit2html('isor_codebook.Rmd')" 
-::Rscript -e "library(knitr); knitr::knit2html('isom_codebook.Rmd')" 
-Rscript -e "library(rmarkdown); render('isor_codebook.Rmd')" 
-Rscript -e "library(rmarkdown); render('isom_codebook.Rmd')" 
-Rscript -e "library(rmarkdown); render('isor_codebook_manuals.Rmd')" 
+::CALL Rscript -e "library(knitr); knitr::knit2html('isor_codebook.Rmd')" 
+::CALL Rscript -e "library(knitr); knitr::knit2html('isom_codebook.Rmd')" 
+CALL Rscript -e "library(rmarkdown); render('isor_codebook.Rmd')" 
+CALL Rscript -e "library(rmarkdown); render('isom_codebook.Rmd')" 
+CALL Rscript -e "library(rmarkdown); render('isor_codebook_manuals.Rmd')" 
 
 REM ===========================================================================
 REM MOVING FILES
 REM ===========================================================================
 
-  Rscript -e "files <- grep('codebook', list.files(pattern='codebook.*pdf$|codebook.*html$'), value=T)" ^
+  CALL  Rscript -e "files <- grep('codebook', list.files(pattern='codebook.*pdf$|codebook.*html$'), value=T)" ^
           -e "file.copy(files, 'Z:/Gesch\u00e4ftsordnungen/Database/aggregats', overwrite=TRUE)" ^
           -e "file.copy(files, 'Z:/Gesch\u00e4ftsordnungen/Database/outputs',   overwrite=TRUE)" ^
           -e "file.copy(files, 'C:/Dropbox/idep/data', overwrite=TRUE)" ^
