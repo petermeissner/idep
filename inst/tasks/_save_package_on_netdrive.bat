7813efbcb43afd34838bcb158b41fc2535061a56
Peter Meissner <retep.meissner@gmail.com>
2015-11-26 08:33:41 +0100

    batch

--- inst/tasks/_save_package_on_netdrive.bat
+++ inst/tasks/_save_package_on_netdrive.bat
@@ -1,3 +1,3 @@
 chcp 1252
-robocopy ../../../idep z:/geschäftsordnungen/database/package/idep /MIR
+robocopy ../../../idep z:/geschäftsordnungen/database/package/idep /MIR 
 robocopy ../../../diffr z:/geschäftsordnungen/database/package/diffr /MIR
